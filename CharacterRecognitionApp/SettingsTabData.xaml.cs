b6f119952e5aedb9aeeef2d7c763d8e8d7724099
Piotrek <piotrek120017@gmail.com>
2018-08-23 15:53:27 +0200

    new tab

---
+++ CharacterRecognitionApp/SettingsTabData.xaml.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace CharacterRecognitionApp
+{
+    /// <summary>
+    /// Logika interakcji dla klasy SettingsTabData.xaml
+    /// </summary>
+    public partial class SettingsTabData : UserControl
+    {
+        public SettingsTabData()
+        {
+            InitializeComponent();
+        }
+
+        private void ButtonSave_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+    }
+}
